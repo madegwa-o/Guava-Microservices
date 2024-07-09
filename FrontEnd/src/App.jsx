bad696f5c93b254623430efc8751fe0e42592067
oscar <oscar@kali.oscar.com>
2024-07-09 11:48:32 +0300

    first commit

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import FetchData from './services/API/FetchData'
+
+function App() {
+
+  return (
+    <div>
+      <FetchData />
+    </div>
+  )
+}
+
+export default App
